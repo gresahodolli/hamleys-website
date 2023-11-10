007d5d9c0994f23a9b9439c1d38c3a9a5d76496e
ArbreshaHodolli <arberesha.hodolli@student.uni-pr.edu>
2023-11-10 17:26:02 +0100

    carousel

--- hamleys/src/App.jsx
+++ hamleys/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/CustomNavbar'
+import HomePage from './components/HomePage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <HomePage />
     </>
   )
 }
