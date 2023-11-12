29a76c60f52fe08fcb92627771518e1036194d19
gresahodolli <gresa.hodolli1@student.uni-pr.edu>
2023-11-12 09:13:36 +0100

    Double Navbar

--- hamleys/src/components/Navbar.jsx
+++ hamleys/src/components/Navbar.jsx
@@ -1,26 +1,42 @@
-function Navbar(){
-    return(
-        <>
-        <nav>
-            <a href="index.html">
-                <svg></svg>
-            </a>
+import React from 'react';
+import './styles/NavbarStyle.css';
 
-            <div>
-                <ul>
-                    <li><i class="bi bi-flag"></i><a href="index.html">Great Britain </a></li>
-                    <li><i class="bi bi-geo-alt"></i><a href="index.html">Find a store</a></li>
-                    <li><i class="bi bi-calendar"></i><a href="index.html">What's on </a></li>
-                    <li><i class="bi bi-truck"></i><a href="index.html">Track Order Shipment</a></li>
-                    <li><i class="bi bi-emoji-laughing"></i><a href="index.html">Sign In / Registe</a></li>
-                    <li><i class="bi bi-bag"></i><a href="index.html">My Bag</a></li>
-                </ul>
-            </div>
-
-
-        </nav>
-        </>
-    )
-}
+const Navbar = () => {
+  return (
+    <div className="double-navbar">
+      {/* First Row */}
+      <div className="first-row">
+        <div className="left-links">
+          <ul>
+          <li><i class="bi bi-flag"></i> Great Britain </li>
+          <li><i class="bi bi-geo-alt"></i> Find a store</li>
+          <li><i class="bi bi-calendar"></i> What's on </li>
+          </ul> 
+        </div>
+        <div className="right-links">
+          <ul>
+            <li><i class="bi bi-truck"></i> Track Order Shipment</li>
+            <li><i class="bi bi-emoji-laughing"></i> Sign In/Register</li>
+            <li><i class="bi bi-bag"></i> My Bag</li>
+          </ul>
+        </div>
+      </div>
+      {/* Second Row */}
+      <div className="second-row">
+        <div className="logo">Logo</div>
+        <div className="dropdown-menu">
+          <ul>
+            <li>Shop toys</li>
+            <li>Christmas</li>
+            <li>By Age</li>
+            <li>By brand</li>
+            <li>Gaming</li>
+            <li>Black Friday</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
