521ccd4898bcbd08fc00216fbb562a86010acc46
gresahodolli <gresa.hodolli1@student.uni-pr.edu>
2023-11-12 13:09:23 +0100

    Fixed warning

--- hamleys/src/components/Navbar.jsx
+++ hamleys/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import './styles/NavbarStyle.css';
 
 const Navbar = () => {
     const [logoSize, setLogoSize] = useState({ width: '100px', height: '50px' });
-    const [logoSrc, setLogoSrc] = useState('https://e7.pngegg.com/pngimages/761/47/png-clipart-hamleys-regent-street-trafford-centre-retail-logo-lakeside-love-miscellaneous.png');
+    const [logoSrc, setLogoSrc] = useState('https://hamleys.co.za/wp-content/uploads/2021/12/cropped-logo-4-2.png');
 
     const handleLogoHover = () => {
         setLogoSrc('https://upload.wikimedia.org/wikipedia/commons/3/38/Hamleys_logo.png');
@@ -21,16 +21,16 @@ const Navbar = () => {
       <div className="first-row">
         <div className="left-links">
           <ul>
-          <li><i class="bi bi-flag"></i> Great Britain </li>
-          <li><i class="bi bi-geo-alt"></i> Find a store</li>
-          <li><i class="bi bi-calendar"></i> What's on </li>
+          <li><i className="bi bi-flag"></i> Great Britain </li>
+          <li><i className="bi bi-geo-alt"></i> Find a store</li>
+          <li><i className="bi bi-calendar"></i> What's on </li>
           </ul> 
         </div>
         <div className="right-links">
           <ul>
-            <li><i class="bi bi-truck"></i> Track Order Shipment</li>
-            <li><i class="bi bi-emoji-laughing"></i> Sign In/Register</li>
-            <li><i class="bi bi-bag"></i> My Bag</li>
+            <li><i className="bi bi-truck"></i> Track Order Shipment</li>
+            <li><i className="bi bi-emoji-laughing"></i> Sign In/Register</li>
+            <li><i className="bi bi-bag"></i> My Bag</li>
           </ul>
         </div>
       </div>
@@ -51,7 +51,6 @@ const Navbar = () => {
               <ul className="dropdown">
                 <li>Deal 1</li>
                 <li>Deal 2</li>
-                {/* Add more dropdown items as needed */}
               </ul>
             </li>
           </ul>
