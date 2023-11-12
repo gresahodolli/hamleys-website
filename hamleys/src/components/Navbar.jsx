4fe34d3b269d6e068a6f1a5ca83ab358d24ae97d
gresahodolli <gresa.hodolli1@student.uni-pr.edu>
2023-11-12 14:54:55 +0100

    Fixing the mess that I've done

--- hamleys/src/components/Navbar.jsx
+++ hamleys/src/components/Navbar.jsx
@@ -2,18 +2,24 @@ import React, { useState } from 'react';
 import './styles/NavbarStyle.css';
 
 const Navbar = () => {
-    const [logoSize, setLogoSize] = useState({ width: '100px', height: '50px' });
-    const [logoSrc, setLogoSrc] = useState('https://hamleys.co.za/wp-content/uploads/2021/12/cropped-logo-4-2.png');
+  const [logo, setLogo] = useState({
+    src: 'https://hamleys.co.za/wp-content/uploads/2021/12/cropped-logo-4-2.png',
+    size: { width: '100px', height: '50px' }
+  });
 
-    const handleLogoHover = () => {
-        setLogoSrc('https://upload.wikimedia.org/wikipedia/commons/3/38/Hamleys_logo.png');
-        setLogoSize({ width: '200px', height: '200px' }); 
-    };
+  const handleLogoHover = () => {
+    setLogo({
+      src: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Hamleys_logo.png',
+      size: { width: '200px', height: '200px' }
+    });
+  };
 
-    const handleLogoLeave = () => {
-        setLogoSrc('https://hamleys.co.za/wp-content/uploads/2021/12/cropped-logo-4-2.png');
-        // setLogoSize({ width: '100px', height: '50px' });
-    };
+  const handleLogoLeave = () => {
+    setLogo({
+      src: 'https://hamleys.co.za/wp-content/uploads/2021/12/cropped-logo-4-2.png',
+      size: { width: '100px', height: '50px' }
+    });
+  };
 
   return (
     <div className="double-navbar">
@@ -21,25 +27,25 @@ const Navbar = () => {
       <div className="first-row">
         <div className="left-links">
           <ul>
-          <li><i class="bi bi-flag"></i> Great Britain </li>
-          <li><i class="bi bi-geo-alt"></i> Find a store</li>
-          <li><i class="bi bi-calendar"></i> What's on </li>
-          </ul> 
+            <li><i className="bi bi-flag"></i> Great Britain </li>
+            <li><i className="bi bi-geo-alt"></i> Find a store</li>
+            <li><i className="bi bi-calendar"></i> What's on </li>
+          </ul>
         </div>
         <div className="right-links">
           <ul>
-            <li><i class="bi bi-truck"></i> Track Order Shipment</li>
-            <li><i class="bi bi-emoji-laughing"></i> Sign In/Register</li>
-            <li><i class="bi bi-bag"></i> My Bag</li>
+            <li><i className="bi bi-truck"></i> Track Order Shipment</li>
+            <li><i className="bi bi-emoji-laughing"></i> Sign In/Register</li>
+            <li><i className="bi bi-bag"></i> My Bag</li>
           </ul>
         </div>
       </div>
       {/* Second Row */}
       <div className="second-row" onMouseEnter={handleLogoHover} onMouseLeave={handleLogoLeave}>
         <div className="logo-container">
-          <img src={logoSrc} alt="Logo" className="logo-image" />
+          <img src={logo.src} alt="Logo" className="logo-image" style={logo.size} />
         </div>
-        <div className="dropdown-menu">
+        <div className="other-items-nav">
           <ul>
             <li>Shop toys</li>
             <li>Christmas</li>
@@ -51,7 +57,6 @@ const Navbar = () => {
               <ul className="dropdown">
                 <li>Deal 1</li>
                 <li>Deal 2</li>
-                {/* Add more dropdown items as needed */}
               </ul>
             </li>
           </ul>
